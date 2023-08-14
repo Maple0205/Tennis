1edbf01df49084bbfb94d5c4ab330a25a67373ec
Nelson <z5382667@student.unsw.edu.au>
2023-08-14 21:09:14 +1000

    second

--- .history/backend/main_20230810194638.go
+++ .history/backend/main_20230810194638.go
@@ -3,12 +3,9 @@ package main
 import (
 	"tennis/conf"
 	"tennis/routes"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
 	conf.Init()
 	r := routes.NewRouter()
 	_ = r.Run(conf.HttpPort)
